7060995e22eb851b688b081d8d9e8500d5997e49
Mohammad Mehrabi <mmehrabi1990@yahoo.com>
2020-10-11 22:23:37 +0200

    Initial commit

---
+++ src/main/java/org/mehrabi/Main.java
@@ -0,0 +1,17 @@
+package org.mehrabi;
+
+import org.apache.commons.lang.ArrayUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        char[] chars = new char[2];
+        ArrayUtils.add(chars,'h');
+        System.out.println( "Hello World!" );
+    }
+}
